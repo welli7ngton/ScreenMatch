f909eee342dbbe77c5840c49cc3a321b558d6d62
welli7ngton <wellingtonasilva45@gmail.com>
2024-03-23 18:31:31 -0300

    removendo comentário

--- src/exercicies/Carro.java
+++ src/exercicies/Carro.java
@@ -1,8 +1,4 @@
 package exercicies;
-// Crie uma classe Carro com métodos para representar um modelo específico ao longo de três anos. Implemente métodos
-// para definir o nome do modelo, os preços médios para cada ano, e calcular e exibir o menor e o maior preço. Adicione
-// uma subclasse ModeloCarro para criar instâncias específicas, utilizando-a na classe principal para definir preços e
-// mostrar informações.
 
 import java.util.ArrayList;
 
