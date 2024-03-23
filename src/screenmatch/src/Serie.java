f933199d0b95958d1d6016a05a2c5389ebcd4fc3
welli7ngton <wellingtonasilva45@gmail.com>
2024-03-23 20:39:52 -0300

    Classes filhas

--- src/screenmatch/src/Serie.java
+++ src/screenmatch/src/Serie.java
@@ -1,2 +1,58 @@
-package screenmatch.src;public class Serie {
+package screenmatch.src;
+
+public class Serie extends Titulo{
+    private int seasons;
+    private boolean active;
+    private int episodesPerSeason;
+    private int minutesPerEpisode;
+
+    private String type = "Serie";
+
+    public int getSeasons() {
+        return seasons;
+    }
+
+    public void setSeasons(int seasons) {
+        this.seasons = seasons;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public int getEpisodesPerSeason() {
+        return episodesPerSeason;
+    }
+
+    public void setEpisodesPerSeason(int episodesPerSeason) {
+        this.episodesPerSeason = episodesPerSeason;
+    }
+
+    public int getMinutesPerEpisode() {
+        return minutesPerEpisode;
+    }
+
+    public void setMinutesPerEpisode(int minutesPerEpisode) {
+        this.minutesPerEpisode = minutesPerEpisode;
+    }
+
+    @Override
+    public int getDurationInMinutes(){
+        return seasons * episodesPerSeason * minutesPerEpisode;
+    }
+
+    @Override
+    public void showTitleInfo(){
+        System.out.println("Type: "+this.type);
+        super.showTitleInfo();
+        System.out.println("Seasons: "+this.getSeasons());
+        System.out.println("Active: "+this.isActive());
+        System.out.println("Epsodes/Season: "+this.getEpisodesPerSeason());
+        System.out.println("Minutes/Episode: "+this.getMinutesPerEpisode());
+    }
+
 }
