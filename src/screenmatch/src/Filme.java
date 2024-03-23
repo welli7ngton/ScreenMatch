4d2fe11f9a15ed8be57ceeb1cf733bbe50a206f1
welli7ngton <wellingtonasilva45@gmail.com>
2024-03-23 20:40:54 -0300

    Filme e Filme recomendação

--- src/screenmatch/src/Filme.java
+++ src/screenmatch/src/Filme.java
@@ -1,2 +1,27 @@
-package screenmatch.src;public class Filme {
+package screenmatch.src;
+
+public class Filme extends Titulo implements Classificavel{
+    private String diretor;
+    private  String type = "Movie";
+
+    public String getDiretor() {
+        return diretor;
+    }
+
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
+    }
+
+
+    @Override
+    public void showTitleInfo(){
+        System.out.println("Type: "+this.type);
+        super.showTitleInfo();
+        System.out.println("Director: "+this.getDiretor());
+    }
+
+    @Override
+    public int getReviewNote() {
+        return (int) this.getClassification();
+    }
 }
--- src/screenmatch/src/FilmeRecomendacao.java
+++ src/screenmatch/src/FilmeRecomendacao.java
@@ -1,5 +1,13 @@
 package screenmatch.src;
 
-public class MovieRecomendation {
-    private String
+public class FilmeRecomendacao {
+    private String recommendation;
+
+    public void filter(Classificavel classificavel){
+        if (classificavel.getReviewNote() >=4){
+            System.out.println("HOT");
+        } else {
+            System.out.println("COLD");
+        }
+    }
 }
