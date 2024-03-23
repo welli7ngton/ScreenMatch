086c98e1728fde62288d29e79a43e6eb70a7a702
welli7ngton <wellingtonasilva45@gmail.com>
2024-03-23 20:40:18 -0300

    criando uma classe para ser classificavel de Serie

--- src/screenmatch/src/Episodio.java
+++ src/screenmatch/src/Episodio.java
@@ -1,2 +1,44 @@
-package screenmatch.src;public class Episodio {
+package screenmatch.src;
+
+import javax.swing.plaf.synth.SynthRadioButtonMenuItemUI;
+
+public class Episodio implements Classificavel{
+    private int number;
+    private String name;
+    private Serie serie;
+
+    private int reviewNote;
+
+    public void setReviewNote(int reviewNote) {
+        this.reviewNote = reviewNote;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Serie getSerie() {
+        return serie;
+    }
+
+    public void setSerie(Serie serie) {
+        this.serie = serie;
+    }
+
+    @Override
+    public int getReviewNote() {
+        return this.reviewNote;
+    }
 }
